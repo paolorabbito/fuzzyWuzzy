2ad9f804792cf681052e904a2f4bc46fe7fdfb33
Paolo Rabbito <47228598+paolorabbito@users.noreply.github.com>
2022-11-24 21:27:18 +0100

    add FuzzyWuzzy.php functionality && test

--- src/FuzzyWuzzy.php
+++ src/FuzzyWuzzy.php
@@ -6,8 +6,50 @@ namespace FuzzyWuzzy;
 
 class FuzzyWuzzy
 {
-    public function say(int $number): string
-    {
-        return 'Hello ';
+
+    public function __construct(
+        private string $firstWord,
+        private string $secondWord,
+        private int $firstDivisor,
+        private int $secondDivisor,
+        private int $thirdDivisor
+    ) {}
+
+    public function getResult(array $numbers): string {
+        $result = '';
+        foreach ($numbers as $number) {
+            if ($number <= 0) {
+                throw new \InvalidArgumentException('Inserita una serie di numeri con almeno un valore minore di 0');
+            }
+
+            $result .= $this->createString($number);
+        }
+
+        return $result;
+    }
+
+    private function createString(int $number): string {
+        if ($number % $this->firstDivisor === 0 && $number % $this->secondDivisor === 0 && $number % $this->thirdDivisor === 0) {
+            return $this->secondWord . $this->firstWord . $this->firstWord . $this->secondWord;
+        }
+        if ($number % $this->firstDivisor === 0 && $number % $this->thirdDivisor === 0) {
+            return $this->secondWord . $this->firstWord . $this->secondWord;
+        }
+        if ($number % $this->secondDivisor === 0 && $number % $this->thirdDivisor === 0) {
+            return $this->firstWord . $this->secondWord . $this->secondWord;
+        }
+        if ($number % $this->firstDivisor === 0 && $number % $this->secondDivisor === 0) {
+            return $this->secondWord . $this->firstWord;
+        }
+        if ($number % $this->firstDivisor === 0) {
+            return $this->firstWord;
+        }
+        if ($number % $this->secondDivisor === 0) {
+            return $this->secondWord;
+        }
+        if ($number % $this->thirdDivisor === 0) {
+            return $this->firstWord . $this->secondWord;
+        }
+        return strval($number);
     }
 }
--- tests/FuzzyWuzzyTest.php
+++ tests/FuzzyWuzzyTest.php
@@ -10,8 +10,47 @@ class FuzzyWuzzyTest extends TestCase
 {
     public function testShouldReturnString(): void
     {
-        $fuzzyWuzzy = new FuzzyWuzzy();
+        $fuzzyWuzzy = new FuzzyWuzzy('Fuzzy', 'Wuzzy', 3, 5, 7);
 
-        $this->assertEquals('Hello ', $fuzzyWuzzy->say(2));
+        $arrayTest = [3, 5, 7, 15, 2, 35];
+
+        $this->assertEquals('FuzzyWuzzyFuzzyWuzzyWuzzyFuzzy2FuzzyWuzzyWuzzy', $fuzzyWuzzy->getResult($arrayTest));
+    }
+
+    public function testShouldReturnFuzzy(): void
+    {
+        $fuzzyWuzzy = new FuzzyWuzzy('Fuzzy', 'Wuzzy', 3, 5, 7);
+
+        $arrayTest = [3];
+
+        $this->assertEquals('Fuzzy', $fuzzyWuzzy->getResult($arrayTest));
+    }
+
+    public function testShouldReturnWuzzy(): void
+    {
+        $fuzzyWuzzy = new FuzzyWuzzy('Fuzzy', 'Wuzzy', 3, 5, 7);
+
+        $arrayTest = [5];
+
+        $this->assertEquals('Wuzzy', $fuzzyWuzzy->getResult($arrayTest));
+    }
+
+    public function testShouldReturnFuzzyWuzzy(): void
+    {
+        $fuzzyWuzzy = new FuzzyWuzzy('Fuzzy', 'Wuzzy', 3, 5, 7);
+
+        $arrayTest = [7];
+
+        $this->assertEquals('FuzzyWuzzy', $fuzzyWuzzy->getResult($arrayTest));
+    }
+
+    public function testShouldThrowError(): void
+    {
+        $fuzzyWuzzy = new FuzzyWuzzy('Fuzzy', 'Wuzzy', 3, 5, 7);
+
+        $arrayTest = [7, 2, -5];
+
+        $this->expectExceptionMessage('Inserita una serie di numeri con almeno un valore minore di 0');
+        $test = $fuzzyWuzzy->getResult($arrayTest);
     }
 }
